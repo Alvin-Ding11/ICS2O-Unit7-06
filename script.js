b766df84f318133870e8485913d3d2fa74ad12d7
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-25 14:33:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let number = 17
+let guess = 0
+
+document.getElementById('start').addEventListener('click', guesser)
+
+function guesser () {
+  while (guess != number) {
+    guess = prompt('Guess a number between 1 and 25')
+    if (guess > number) {
+      alert('To Big! Try Again!')
+    } else if (guess < number) {
+      alert('To Small! Try Again!')
+    } else {
+      alert('Congratulations! You guessed the right number')
+    }
+  }
+}
